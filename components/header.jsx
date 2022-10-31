bd27a31c564e3366e3ba6466461a482a33993c62
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-10-31 21:51:39 +0900

    共通のレイアウトを元にヘッダーにスタイルを適応

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,10 @@
 import Logo from "./logo";
 import Nav from "./nav";
+import styles from "styles/header.module.scss";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.flexContainer}>
       <Logo boxOn />
       <Nav />
     </header>
